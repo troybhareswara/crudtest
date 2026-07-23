b160435bb5328e95223ed698ff40838cee613bb6
troybhareswara <troybhareswara@gmail.com>
2026-07-24 03:18:03 +0800

    Update database connection configuration to use '127.0.0.1' and set default values for user, password, and database

--- lib/db.ts
+++ lib/db.ts
@@ -1,11 +1,11 @@
 import mysql from 'mysql2/promise';
 
 export const db = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: Number(process.env.DB_PORT) || 3306, // 👈 Harus Number()
+  host: process.env.DB_HOST || '127.0.0.1', // 👈 Ganti dari 'localhost' jadi '127.0.0.1'
+  user: process.env.DB_USER || 'u317479555_test',
+  password: process.env.DB_PASSWORD || '6;YqmINzsv=',
+  database: process.env.DB_NAME || 'u317479555_db_crudtest',
+  port: Number(process.env.DB_PORT) || 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
